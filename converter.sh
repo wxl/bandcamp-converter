d9e26100c950c1c483aed7e1c4bcdb14b64866e2
Walter Lapchynski <carsrcoffins23@yahoo.com>
2014-05-02 08:44:26 -0700

    removed quoting, added path arg

--- converter.sh
+++ converter.sh
@@ -2,25 +2,32 @@
 
 # tool to render file acceptable for Bandcamp
 
+if [ $# -ne 1 ]; then
+	echo "Usage: $(basename $0) '/path/to/files'"
+	exit 1
+fi
+
+pathtofiles=$1
+
 # convert lossy-- add to list as needed
-for file in "$(find . -regextype posix-extended -iregex ".*(mp3|ogg)")"; do
-	convert "${file}" "${file}"-converted.flac
+for file in "$(find "${pathtofiles}"-regextype posix-extended -iregex ".*(mp3|ogg)")"; do
+	convert "${pathtofiles}${file}" "${pathtofiles}${file}"-converted.flac
 done
 
 # convert lossless sample rate and precision as needed
-for file in "$(find . -regextype posix-extended -iregex ".*(flac|wav|aiff)")"; do 
+for file in "$(find "${pathtofiles}" -regextype posix-extended -iregex ".*(flac|wav|aiff)")"; do 
 	# is sample rate high enough?
-	if [ "$(soxi "${file}" | grep "Sample Rate" | awk '{ print $4 }')" -lt "44100" ]; then
+	if [ "$(soxi "${pathtofiles}${file}" | grep "Sample Rate" | awk '{ print $4 }')" -lt "44100" ]; then
 		# is precision also high enough?
-		if [ "$(soxi "${file}" | grep "Precision" | awk '{ gsub ( /-bit/, "" ); print $3 }')" -lt "16" ]; then
+		if [ "$(soxi "${pathtofiles}${file}" | grep "Precision" | awk '{ gsub ( /-bit/, "" ); print $3 }')" -lt "16" ]; then
 			# if no to both, then convert both
-			sox "${file}" -b 16 -r 44100 "${file}"-upsampled-higher_bit.flac
+			sox "${pathtofiles}${file}" -b 16 -r 44100 "${pathtofiles}${file}"-upsampled-higher_bit.flac
 		# if not precision, must be sample rate
 		else
 			sox "${file}" -r 44100 "${file}"-upsampled.flac
 		fi
 	# if sample rate is ok, check precision
-	elif [ "$(soxi "${file}" | grep "Precision" | awk '{ gsub ( /-bit/, "" ); print $3 }')" -lt "16" ]; then
-		sox "${file}" -b 16 "${file}"-upsampled-higher_bit.flac
+	elif [ "$(soxi "${pathtofiles}${file}" | grep "Precision" | awk '{ gsub ( /-bit/, "" ); print $3 }')" -lt "16" ]; then
+		sox "${pathtofiles}${file}" -b 16 "${pathtofiles}${file}"-upsampled-higher_bit.flac
 	fi
 done
